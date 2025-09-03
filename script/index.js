5af31c30e2f867f1f4b4ef07eaf5a8b190b6b7a0
Imran1476 <imranlf2028@gmail.com>
2025-09-04 05:19:12 +0600

    added  lesson part

--- script/index.js
+++ script/index.js
@@ -7,8 +7,36 @@ fetch("https://openapi.programming-hero.com/api/levels/all")
     displayLessons(json.data);
 })
 }
+const loadLevelWord=(id)=>{
+    const url= `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+    .then(response=>response.json())
+    .then(json=> displayLevelWord(json.data));
 
+}
+const displayLevelWord=(words)=>{
+   const wordContainer=document.getElementById("word-container");
+ wordContainer.innerHTML="";
+  words.forEach(word=>{
+    console.log(word)
+    const card=document.createElement("div");
+    card.innerHTML=`
+    <div class="bg-white py-10 px-8 text-center rounded-lg shadow-lg">
+    <h2 class="text-3xl font-bold mb-5">Eager</h2>
+<p class="text-2xl ">Meaning /Pronounciation</p><br>
+<div class="font-bangla text-3xl">"আগ্রহী / ইগার"</div>
 
+<div class="flex justify-between items-center">
+
+    <button class="btn bg-[#1a91ff1a]"><i class="fa-solid fa-circle-info"></i></button>
+    <button class="btn  bg-[#1a91ff1a]"><i class="fa-solid fa-volume-high"></i></button>
+</div>
+
+</div>
+    `;
+    wordContainer.append(card);
+  })
+}
 const displayLessons=(lessons)=>{
    
 // 1. get element.....
@@ -19,7 +47,8 @@ lessons.forEach(lesson=>{
     console.log(lesson);
 const btnDiv=document.createElement("div");
 btnDiv.innerHTML=  `
-  <button class="btn btn-outline btn-primary" ><i class="fa-solid fa-book-open"></i>Lesson-${lesson.level_no
+  <button onclick="loadLevelWord(${lesson.level_no
+})" class="btn btn-outline btn-primary" ><i class="fa-solid fa-book-open"></i>Lesson-${lesson.level_no
 }</button>
 `;
 lessonContainer.append(btnDiv);
